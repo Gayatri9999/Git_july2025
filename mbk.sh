565b820477f3f1ca2b38d6a393a6eb5c5d821e5f
Gayatri9999 <130549839+Gayatri9999@users.noreply.github.com>
2025-08-03 22:23:06 +0530

    Update mbk.sh

---
+++ mbk.sh
@@ -0,0 +1,3 @@
+The 'git pull' command updates the current local branch with changes from a remote repository and combining it with a local repository.
+"git pull is a command used to fetch changes from a remote repository and immediately merge them into your current local branch. It’s a quick way to sync your work with the latest updates that others have pushed"
+
